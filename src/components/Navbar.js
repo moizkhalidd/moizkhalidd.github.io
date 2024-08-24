1e14a4108eb741ecd1719feeffc8791f56e3b027
moizkhalidd <mmoizk2004@gmail.com>
2024-08-25 04:43:00 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import logo3 from "../Assets/logo3.png";
-import tablogo from "../Assets/tablogo.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
