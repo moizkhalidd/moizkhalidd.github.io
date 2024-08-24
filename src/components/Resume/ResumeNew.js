5492dd1ea7f57ec87b140d64cb4367c4040509ca
moizkhalidd <mmoizk2004@gmail.com>
2024-08-25 02:26:47 +0500

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -33,7 +33,7 @@ function ResumeNew() {
 
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page  scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
