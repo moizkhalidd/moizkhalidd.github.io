81a2c7cb3264b0df071031952d1be7f2883cb41c
moizkhalidd <mmoizk2004@gmail.com>
2024-08-25 04:30:39 +0500

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -33,7 +33,7 @@ import usefp from "../../Assets/usefp.png";
            </Col>
            <Col
              md={5}
-             style={{ paddingTop: "120px", paddingBottom: "50px" }}
+             style={{ paddingTop: "60px", paddingBottom: "50px" }}
              className="about-img"
            >
              <img src={me} alt="about" className="img-fluid" />
