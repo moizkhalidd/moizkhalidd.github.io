9ce80fdf7c80c610de2f19fdb81188a1be115dc9
moizkhalidd <mmoizk2004@gmail.com>
2024-08-25 16:10:25 +0500

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -57,43 +57,10 @@ function Projects() {
              demoLink="https://pantry-tracker-saad-c2lurmru2-saad-ahmed-ranas-projects.vercel.app/"              
             />
           </Col>
-/*
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={MATLAB}
-              alt="Saad Ahmed Rana"
-              isBlog={false}
-              title="Para thrust"
-              description="This application, developed in the MATLAB environment, performs a parametric and performance analysis of a turbo-jet engine. The project was funded by the government of Pakistan and completed according to the guidelines of the NASA Systems Engineering Handbook. "
-              ghLink="https://drive.google.com/drive/folders/1hZ8tWqEDbD2QUUOj0CAZp9JsIepPHJ6K?usp=drive_link"
-              //demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={moveIT}
-              alt="Saad Ahmed Rana"
-              isBlog={false}
-              title="Path planning in ROS2"
-              description="This project plans the trajectory of a robot URDF model for domestic robot as it picks up objects and places them in a pre-decided place. moveIT is used in ROS2 to visualize in cartesian coordinates and then the code runs the simulation in quaternion coordinates in the background. "
-              ghLink="https://drive.google.com/drive/folders/1slVHcb_GYZP8J9f-uVvBsdKfZLr8xg6w"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-                imgPath={openCV}
-                alt="Saad Ahmed Rana"
-                isBlog={false}
-                title="Computer Vision"
-                description="This is a hand detection and tracking module that counts the number of fingers open for any number of hands on the stream. The project is being extended to detect sign language gestures for making a sign-to-speech model."
-                ghLink="https://drive.google.com/drive/folders/1MkFPLrYuMmITa5wryXUymGWyD_lU5MKk?usp=drive_link"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
-  */ 
+
+                  
         </Row>  
         
 /*
