119c194bc0c25a9af7cf1a41895d9b1a5e4e1264
moizkhalidd <mmoizk2004@gmail.com>
2025-01-18 22:04:35 +0500

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -22,7 +22,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={pdf} 
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
