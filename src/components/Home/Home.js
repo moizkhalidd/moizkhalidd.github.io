606aee32ac50479f6533aa85d6ec10dd29615d2a
moizkhalidd <mmoizk2004@gmail.com>
2025-01-18 22:18:11 +0500

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ import Home2 from "./Home2";
 import Type from "./Type";
 import moiz from "../../Assets/Projects/moiz.jpg"; // Import the image
 
-function Home() {
+function Home() { 
   return (
     <section>
       <Container fluid className="home-section" id="home">
