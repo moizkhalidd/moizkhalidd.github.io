e957ce80a80be67ff1ed4fe722a06fad5c965560
Moiz Khalid <mmoizk2004@gmail.com>
2025-06-27 19:46:00 +0500

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,7 +7,7 @@ function Type() {
       options={{
         strings: [
           "Bachelors of Data Science @ FAST",
-          "Dean's list - Fall 23, Spring 24 & Fall 24",
+          "Dean's list - Fall 23, Spring 24, Fall 24 & Spring 25",
           "Football Enthusiast",
         ],
         autoStart: true,
