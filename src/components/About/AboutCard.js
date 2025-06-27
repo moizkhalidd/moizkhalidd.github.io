e7ed4ff1935a710ad4aba08b9405931818ce8fb0
Moiz Khalid <mmoizk2004@gmail.com>
2025-06-27 20:17:42 +0500

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
-
+import { BsDot } from "react-icons/bs";
  function AboutCard() {
    return (
      <Card className="quote-card-view">
