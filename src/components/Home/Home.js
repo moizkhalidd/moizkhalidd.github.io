5ed9df3d04f7f7c81b411062c15d23433b21036e
Moiz Khalid <mmoizk2004@gmail.com>
2025-06-27 20:05:09 +0500

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -26,9 +26,7 @@ function Home() {
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
-                <div style={{ maxWidth: "400px", wordWrap: "break-word", lineHeight: "1.6" }}>
                 <Type />
-                </div>  
               </div>
             </Col>
 
