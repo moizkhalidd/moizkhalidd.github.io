21ccb6580cf2fd615038bdb1da6839c35030108d
Moiz Khalid <mmoizk2004@gmail.com>
2026-07-17 00:52:10 -0500

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -64,13 +64,15 @@ import zayed from "../../Assets/zayed.png";
             <span className="purple">Published in IEEE Xplore</span>
             <br />
           </p>
-          <Button
-            variant="primary"
-            href="https://ieeexplore.ieee.org/document/11576503"
-            target="_blank"
-          >
-            {"Publication Link"}
-          </Button>    
+          <div style={{ position: "relative", zIndex: 10 }}>
+           <Button
+             variant="primary"
+             href="https://ieeexplore.ieee.org/document/11576503"
+             target="_blank"
+           >
+             Publication Link
+           </Button>
+         </div>  
         </div>
         
         <div style={{ marginBottom: "30px" }}>
